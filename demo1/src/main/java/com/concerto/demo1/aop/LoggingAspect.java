19e44568571aeb24b270635b8f2a0029be43a903
Gayatri Hande <121214507+gayatriCodes23@users.noreply.github.com>
2023-09-15 18:38:11 +0530

    Add files via upload

--- demo1/src/main/java/com/concerto/demo1/aop/LoggingAspect.java
+++ demo1/src/main/java/com/concerto/demo1/aop/LoggingAspect.java
@@ -4,6 +4,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,13 +14,35 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
     private final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @AfterReturning(pointcut = "execution(* com.concerto.demo1.controller.TestController.*(..))", returning = "result")
+//    @AfterReturning(pointcut = "execution(* com.concerto.demo1.controller.TestController.*(..))", returning = "result")
+//    public void logMethodCall(JoinPoint joinPoint, Object result) {
+//        String methodName = joinPoint.getSignature().getName();
+//        logger.info("Method '{}' returned: {}", methodName, result);
+//    }
+//    
+//    
+//    @AfterThrowing(pointcut = "execution(* com.concerto.demo1.*.*.*(..))", throwing = "exception")
+//    public void handleException(JoinPoint joinPoint, Exception exception) {
+//        String methodName = joinPoint.getSignature().getName();
+//        logger.error("Method '{}' threw an exception: {}", methodName, exception.getMessage());
+//        // You can add additional error handling logic here if needed
+//    }
+    
+    
+
+    // Define the pointcut for methods within com.concerto.demo1.controller.TestController
+    @Pointcut("execution(* com.concerto.demo1.controller.TestController.*(..))")
+    public void testControllerMethods() {
+    }
+
+    // AfterReturning advice for successful method execution
+    @AfterReturning(pointcut = "testControllerMethods()", returning = "result")
     public void logMethodCall(JoinPoint joinPoint, Object result) {
         String methodName = joinPoint.getSignature().getName();
         logger.info("Method '{}' returned: {}", methodName, result);
     }
-    
-    
+
+    // AfterThrowing advice for handling exceptions
     @AfterThrowing(pointcut = "execution(* com.concerto.demo1.*.*.*(..))", throwing = "exception")
     public void handleException(JoinPoint joinPoint, Exception exception) {
         String methodName = joinPoint.getSignature().getName();
@@ -27,3 +50,5 @@ public class LoggingAspect {
         // You can add additional error handling logic here if needed
     }
 }
+    
+
